849fd1c4deef7137d404ff078972063c99fefd17
meeshlin <meesh.lin@icloud.com>
2016-05-12 16:56:04 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  //Set up our variables to use later
+  var computerChoice;
+
+  //Get a random value for the computer choice
+  var computerChoiceVal = Math.random();
+
+  //Turn the random value into a choice for the computer
+  if (computerChoiceVal < 0.34) {
+    computerChoice = "rock";
+  } else if (computerChoiceVal <= 0.67 && computerChoiceVal > 0.34) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  //Player
+  $(document).on("click", ".choice", function() {
+    var playerChoice = $(this).attr("id");
+
+    if (playerChoice == computerChoice) {
+      alert ("It's a tie!");
+    } else if (playerChoice == "rock") {
+      if (computerChoice == "scissors") {
+        //Alert ("You win!");
+        $(this).addClass("success green-background");
+      } else {
+        alert ("You lose :(");
+      };
+    } else if (playerChoice == "paper") {
+      if (computerChoice == "rock") {
+        //Alert ("You win!");
+        $(this).addClass("success green-background");
+      } else {
+        alert ("You lose :(");
+      };
+    } else if (playerChoice == "scissors") {
+      if (computerChoice == "paper") {
+        $(this).addClass("success green-background");
+      } else {
+        alert ("You lose :(");
+      };
+    }
+  });
+});
